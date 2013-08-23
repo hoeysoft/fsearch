d3e6c155987a2afe89a6dd9fc95bdf012f4fe77c
hoey <nznial@gmail.com>
2013-08-23 14:34:54 +0900

    "almost done"

--- slg.py
+++ slg.py
@@ -1,7 +1,7 @@
 # slg.py - Search Launcher Generator
 # Written by hoey.
 
-"""Usage : >>python slg.py QUERY_URL [, OUTNAME]
+"""Usage : >>python slg.py 
 """
 
 _TEMPLATE = \
@@ -9,12 +9,12 @@ _TEMPLATE = \
 
 ::URL to query
 SET HOEY_LCHR_QUERY="{0}"
-SET HOEY_LCHR_QUERY=%HOEY_LCHR_QUERY:"=%
 SET HOEY_LCHR_QUERY=%HOEY_LCHR_QUERY%%1
 SHIFT
 
 ::Extract default browser path(Windows7 Based)
 FOR /F "tokens=2* delims=	 " %%A IN ('REG QUERY "HKEY_CLASSES_ROOT\http\shell\open\command" /ve') DO SET HOEY_LCHR_BROWSER=%%B
+
 
 :Loop
 IF "%1"=="" GOTO Continue
@@ -25,8 +25,14 @@ SHIFT
 GOTO Loop
 :Continue
 
+
+:: Adjust quotation mark.
+:: "url.com/abc?query="abc ===> "url.com/abc?query=abc"
+SET HOEY_LCHR_QUERY="%HOEY_LCHR_QUERY:"=%"
+
 %HOEY_LCHR_BROWSER% %HOEY_LCHR_QUERY%
 
+:: Release env variable.
 SET HOEY_LCHR_BROWSER=
 SET HOEY_LCHR_QUERY=
 """
@@ -35,25 +41,34 @@ SET HOEY_LCHR_QUERY=
 _OUT_EXTENSION = '.bat'
 
 import sys
+import re
 if __name__ == '__main__':
-    argc = len(sys.argv)
-    assert argc > 1, 'Should be specified QUERY_URL at least.'
-    assert argc < 4, 'Should be specified QUERY_URL, OUTFILE at most.'
-
-    query_url = sys.argv[1]
-    outfile = 'out.bat'
-    if argc == 3:
-        outfile = sys.argv[2]
-        if outfile[-4:] != _OUT_EXTENSION:
-            outfile = outfile + _OUT_EXTENSION 
-    
     try:
-        f = open(outfile, 'w')
-        f.write(_TEMPLATE.format(query_url))
-        f.close()
+        while True:
+            template = _TEMPLATE[:]
+
+            line = raw_input()
+            args = re.split('\s*', line, 2)
+            query_url = args[0]
+            assert(query_url != '')
+
+            if len(args) > 1: outfile = args[1]
+            else:             outfile = 'out.bat'
+
+            if outfile[-4:] != _OUT_EXTENSION:
+                outfile = outfile + _OUT_EXTENSION 
+    
+            f = open(outfile, 'r')
+            f.write(template.format(query_url))
+            f.close()
+            
+            print(query_url + ' ==> ' + outfile)
+
+    except EOFError:
+        pass
+
     except:
-        print('File error occured.')
+        sys.stderr.write('error occurred.\n')
 
 else:
     print("Can't use 'slg.py' as a module")
-
